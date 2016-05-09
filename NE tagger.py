c6439c95c050c8f36e99bfd7b1f41ba66c5b5736
tjarles <chafo246@student.liu.se>
2016-05-09 15:47:33 +0200

    Add files via upload

--- NE tagger.py
+++ NE tagger.py
@@ -1,11 +1,17 @@
 from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
 
 #Global variable that buttons can change before printing to file
 NE = ""
+isSaveMode = False
 
 class Windows:
     def __init__(self, firstWord, secondWord, thirdWord):
         self.Master=Tk()
+
+        #Set default closing action to call on save-mode.
+        self.Master.protocol("WM_DELETE_WINDOW", self.ExitAndSave)
 
         #Creates labelbox containing three cooccuring words
         self.labelbox = LabelFrame(self.Master, text="Möjlig entitet:").grid_anchor(CENTER)
@@ -18,16 +24,32 @@ class Windows:
         self.ne = Label(self.labelbox, text=NE, font = "Helvetica 12 bold").grid(row = 1, column = 1)
 
         #Keybindings
-        self.Master.bind('m', lambda x: self.Set("MEDICIN"))
-        self.Master.bind('s', lambda x: self.Set("SJUKDOM"))
-        self.Master.bind('b', lambda x: self.Set("BEHANDLING"))
-        self.Master.bind('k', lambda x: self.Set("KEEP"))
+        self.Master.bind('1', lambda x: self.Set("sjukdom"))
+        self.Master.bind('2', lambda x: self.Set("medicin"))
+        self.Master.bind('3', lambda x: self.Set("åtgärd"))
+        self.Master.bind('4', lambda x: self.Set("kroppsdel"))
+        self.Master.bind('7', lambda x: self.Set("verktyg"))
+        self.Master.bind('8', lambda x: self.Set("mottagning"))
+        self.Master.bind('9', lambda x: self.Set("person"))
+        self.Master.bind('0', lambda x: self.Set("organisation"))
+        self.Master.bind('+', lambda x: self.Set("REPLACEWITH_"))
 
         #Buttons
-        self.Button=Button(self.Master,text="Medicin",command=lambda: self.Set("MEDICIN")).grid(row = 2, column = 0)
-        self.Button=Button(self.Master,text="Sjukdom",command=lambda: self.Set("SJUKDOM")).grid(row = 2, column = 1)
-        self.Button=Button(self.Master,text="Behandling",command=lambda:self.Set("BEHANDLING")).grid(row = 2, column = 2)
-        self.Button=Button(self.Master,text="Keep Current",command=lambda:self.Set("KEEP")).grid(row = 2, column = 3)
+        self.Button=Button(self.Master,text="Sjukdom (1)",command=lambda: self.Set("sjukdom")).grid(row = 2, column = 0)
+        self.Button=Button(self.Master,text="Medicin (2)",command=lambda: self.Set("medicin")).grid(row = 2, column = 1)
+        self.Button=Button(self.Master,text="Åtgärd (3)",command=lambda:self.Set("åtgärd")).grid(row = 3, column = 0)
+        self.Button=Button(self.Master,text="Kroppsdel (4)",command=lambda:self.Set("kroppsdel")).grid(row = 3, column = 1)
+
+        #Creates space between buttons
+        self.Label=Label(self.Master,text="             ").grid(row = 2, column = 3)
+
+        self.Button=Button(self.Master,text="Verktyg (7)",command=lambda: self.Set("verktyg")).grid(row = 2, column = 4)
+        self.Button=Button(self.Master,text="Mottagning (8)",command=lambda: self.Set("mottagning")).grid(row = 2, column = 5)
+        self.Button=Button(self.Master,text="Person (9)",command=lambda:self.Set("person")).grid(row = 3, column = 4)
+        self.Button=Button(self.Master,text="Organisation (0)",command=lambda:self.Set("organisation")).grid(row = 3, column = 5)
+
+        self.Button=Button(self.Master,text="Leave unchanged (+)",command=lambda:self.Set("REPLACEWITH_")).grid(row = 4, column = 5)
+        self.Button=Button(self.Master,text="Exit and save",command=lambda:self.ExitAndSave()).grid(row = 6, column = 5)
         self.Master.mainloop()
 
     #Function setting the NE and destroying window
@@ -36,12 +58,27 @@ class Windows:
         NE = entityLabel
         self.Master.destroy()
 
+    def ExitAndSave(self):
+        result = messagebox.askokcancel("Exit and save confirmation", "Are you sure you want to save and quit? You can pickup where you left by opening this file again.")
+        #If answer "ok" enter savemode and close screen
+        if result:
+            global isSaveMode
+            isSaveMode = True
+            self.Master.destroy()
 
-f2 = open("NE-tagged_blogg_corpus.conll", "w")
+#File to save to
+f2 = open("NE-tagged_blogg_corpus9.conll", "w")
 
-with open('blogg_corpus.conll', "r",  encoding='utf-8') as f:
+#For letting user chose file in the future
+# with open(filedialog.askopenfilename(), "r",  encoding='utf-8') as f:
+
+#File to read from
+with open('NE-tagged_blogg_corpus8.conll', "r",  encoding='utf-8') as f:
     threeCurrentWords = ["",""]
     for line in f:
+        if isSaveMode:
+            f2.write(line)
+            continue;
         if line == "\n":
             f2.write("\n")
         if line != "\n":
@@ -58,9 +95,12 @@ with open('blogg_corpus.conll', "r",  encoding='utf-8') as f:
             threeCurrentWords.append(lineList[1])
 
             #Windows() takes input from the user and assigns that as NE
-            if lineList[3] == "PM":
-                Windows(firstWord = threeCurrentWords[0], secondWord = threeCurrentWords[1], thirdWord = threeCurrentWords[2])
-                lineList[-2] = NE
+            try:
+                if (lineList[3] == "PM" or lineList[3] == "NN") and NE == "_":
+                    Windows(firstWord = threeCurrentWords[0], secondWord = threeCurrentWords[1], thirdWord = threeCurrentWords[2])
+                    lineList[-2] = NE
+            except IndexError:
+                print(line)
 
             newLine = ""
             for column in lineList:
